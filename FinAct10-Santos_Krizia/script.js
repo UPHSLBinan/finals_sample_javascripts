9a677fe426abae754f103327ff27c2ce01be4273
kriziasantos <kriziacarisse.santos@gmail.com>
2023-04-24 18:13:53 +0800

    Calculator_Santos

--- FinAct10-Santos_Krizia/script.js
+++ FinAct10-Santos_Krizia/script.js
@@ -32,18 +32,16 @@ function changeBackgroundImage() {
   body.style.backgroundImage = `url('${randomImage}')`;
 }
 function getRandomImage() {
-    // Array of background image URLs
+    
     let images = [
-      'https://e1.pxfuel.com/desktop-wallpaper/183/423/desktop-wallpaper-mystical-stylised-lonely-mountain-vista-1920x1200-mystical-moonlight.jpg', // Replace with your image URL
-      'https://wallpaper.dog/large/238738.png', // Replace with your image URL
-      'https://e1.pxfuel.com/desktop-wallpaper/768/284/desktop-wallpaper-pastel-purple-computer-light-purple-aesthetic-pc.jpg', // Replace with your image URL
+      'https://e1.pxfuel.com/desktop-wallpaper/183/423/desktop-wallpaper-mystical-stylised-lonely-mountain-vista-1920x1200-mystical-moonlight.jpg', 
+      'https://wallpaper.dog/large/238738.png', 
+      'https://e1.pxfuel.com/desktop-wallpaper/768/284/desktop-wallpaper-pastel-purple-computer-light-purple-aesthetic-pc.jpg', 
       'https://wallpapercave.com/wp/wp7952670.jpg',
       'https://static.vecteezy.com/system/resources/previews/009/925/006/original/cute-purple-aesthetic-abstract-minimal-background-perfect-for-wallpaper-backdrop-postcard-background-vector.jpg'
-      // Add more image URLs here
     ];
-    // Get a random index
+   
     let randomIndex = Math.floor(Math.random() * images.length);
-    // Return the randomly selected image URL
     return images[randomIndex];
   }
   
