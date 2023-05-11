f477304b7792dcd0fc22a6190948df1d73079168
Junella Agatha B. Magalong <scndella1@gmail.com>
2023-05-12 00:48:21 +0800

    .

---
+++ slotmachine_Magalong/slot.js
@@ -0,0 +1,13 @@
+var slot1 = document.getElementById("slot1");
+var slot2 = document.getElementById("slot2");
+var slot3 = document.getElementById("slot3");
+
+var symbols = ["★", "♥", "✦", "✿", "☀"]; 
+var wins = {
+    "★★★": 2,
+    "♥♥♥": 5,
+    "✦✦✦": 15,
+    "✿✿✿": 20,
+    "☀☀☀": 100
+};
+
